5c6fbb942c833a00ccad7d88ba47546b4bca6d6e
avil13 <13.avil@gmail.com>
2017-01-13 01:09:38 +0300

    pages index2

---
+++ src/app/parts/index-bg/index-bg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-index-bg',
+  templateUrl: './index-bg.component.html',
+  styleUrls: ['./index-bg.component.scss']
+})
+export class IndexBgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
